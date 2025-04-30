29a5f55c47558af9c7d6c075af40bfb454b81ecb
Dawn Umahi <dawn.umahi@bison.howard.edu>
2025-04-30 20:50:44 +0000

    Main method

--- mycipher.py
+++ mycipher.py
@@ -16,4 +16,14 @@ def format_blocks(text, block_size = 5, blocks_per_line=10):
     for i in range(0, len(blocks), blocks_per_line):
         print(' '.join(blocks[i:i+blocks_per_line]))
 
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 mycipher.py <shift>")
+    shift = int(sys.argv[1]) % 26
 
+    plaintext = sys.stdin.read()
+    cipher = caesar(plaintext, shift)
+    format_blocks(cipher)
+
+if __name__ = "__main__":
+    main()
