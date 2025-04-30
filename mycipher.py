763e0dfd4ebd684ca46de45102a1e9ac83e5947b
Dawn Umahi <dawn.umahi@bison.howard.edu>
2025-04-30 20:38:14 +0000

    Caesar function

--- mycipher.py
+++ mycipher.py
@@ -2,4 +2,10 @@
 import sys
 import string
 
-
+def caesar(s, shift):
+    out = []
+    for char in s.upper():
+        if char in string.ascii_uppercase:
+            temp = (ord(char) - ord('A') + shift) % 26
+            out.append(chr(ord('A') + temp))
+    return ''.join(out)
