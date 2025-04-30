ca4df346657cd48d0ed81ac842ef7ec6bcd855e3
Dawn Umahi <dawn.umahi@bison.howard.edu>
2025-04-30 20:44:47 +0000

    Caesar function

--- mycipher.py
+++ mycipher.py
@@ -9,3 +9,9 @@ def caesar(s, shift):
             temp = (ord(char) - ord('A') + shift) % 26
             out.append(chr(ord('A') + temp))
     return ''.join(out)
+
+def format_blocks(text, block_size = 5, blocks_per_line=10):
+    blocks = [ text[i:i+block_size]
+            for i in range(0, len(text), block_size)]
+    for i in range(0, len(blocks), blocks_per_line):
+        print(' '.join(blocks[i:i+blocks_per_line]))
